9ac4463506a3d3d5ee8b8c21621a21aabd2f36ea
Professional <odin5858@gmail.com>
2024-09-26 22:09:07 +0300

    Задание 2.2

--- Lab2/FileName.cpp
+++ Lab2/FileName.cpp
@@ -81,6 +81,7 @@
 	//Время выполнения при размере матрицы  2000 87,918 секунд
 
 }*/
+/*
 #include<iostream>
 #include<cstdlib>
 #include<time.h>
@@ -115,7 +116,7 @@ void qs(int* items, int left, int right) //вызов функции: qs(items, 0, count-1);
 
 	i = left; j = right;
 
-	/* выбор компаранда */
+	// выбор компаранда 
 	x = items[(left + right) / 2];
 
 	do {
@@ -193,6 +194,106 @@ int main()
 	}
 	cout << cpu_time_used << "секунд" << endl;
 	delete[]items;
+
+	system("pause");
+}
+*/
+
+///Задание 2
+
+
+
+
+#include<iostream>
+#include<cstdlib>
+#include<time.h>
+using namespace std;
+
+
+void shell(int* items, int count)
+{
+
+	int i, j, gap, k;
+	int x, a[5];
+
+	a[0] = 9; a[1] = 5; a[2] = 3; a[3] = 2; a[4] = 1;
+
+	for (k = 0; k < 5; k++) {
+		gap = a[k];
+		for (i = gap; i < count; ++i) {
+			x = items[i];
+			for (j = i - gap; (x < items[j]) && (j >= 0); j = j - gap)
+				items[j + gap] = items[j];
+			items[j + gap] = x;
+		}
+	}
+}
+
+void qs(int* items, int left, int right) //вызов функции: qs(items, 0, count-1);
+{
+	int i, j;
+	int x, y;
+
+
+
+	i = left; j = right;
+
+	// выбор компаранда
+	x = items[(left + right) / 2];
+
+	do {
+		while ((items[i] < x) && (i < right)) i++;
+		while ((x < items[j]) && (j > left)) j--;
+
+		if (i <= j) {
+			y = items[i];
+			items[i] = items[j];
+			items[j] = y;
+			i++; j--;
+		}
+	} while (i <= j);
+
+	if (left < j) qs(items, left, j);
+	if (i < right) qs(items, i, right);
+}
+
+
+
+int main() {
+	///Задание 2.2
+	srand(time(NULL));
+	setlocale(LC_ALL, "RUS");
+
+	clock_t start, end; 
+	double cpu_time_used;
+
+	int count = 10000; 
+	int* items = new int[count]; 
+
+
+	for (int i = 0; i < count; i++) {
+		items[i] =  i+1 ;
+		cout << items[i] << endl;
+	}
+
+
+	start = clock();
+	shell(items, count); 
+	end = clock();
+
+
+	cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+	cout << "Время выполнения: " << cpu_time_used << " секунд" << endl;
+	start = 0;
+	end = 0;
+	start = clock();
+	qs(items, 0, count - 1);
+	
+	end = clock();
+	cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+	cout << "Время выполнения: " << cpu_time_used << " секунд" << endl;
+
+	delete[] items;  
 
 	system("pause");
 }
